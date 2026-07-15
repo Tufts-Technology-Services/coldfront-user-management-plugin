02fa726c22c73a7263852ae2441d50dd05eb87e8
Chris Barnett <Christopher.Barnett@tufts.edu>
2026-07-15 09:45:05 -0400

    use list instead of set for dict of user info

--- user_management/views.py
+++ user_management/views.py
@@ -9,11 +9,11 @@ from user_management.utils import get_project_group_members_from_external
 @csrf_protect
 @user_passes_test(lambda u: u.is_superuser)
 def get_group_members(request, project_id) -> list[str]:
-    members = set() 
+    members = [] 
     group, ext_members = get_project_group_members_from_external(project_id)
     project_users = ProjectUser.objects.filter(project_id=project_id, status__name="Active").distinct()
     if project_users.exists():
         for pu in project_users:
-            members.add({"user": {"username": pu.user.username, "first_name": pu.user.first_name, "last_name": pu.user.last_name, "email": pu.user.email}, "source_match": pu.user.username in ext_members})
+            members.append({"user": {"username": pu.user.username, "first_name": pu.user.first_name, "last_name": pu.user.last_name, "email": pu.user.email}, "source_match": pu.user.username in ext_members})
     missing_members = ext_members - set(project_users.values_list("user__username", flat=True))
-    return JsonResponse({"members": list(members), "group": group, "missing_users": list(missing_members)}, status=200)
+    return JsonResponse({"members": members, "group": group, "missing_users": list(missing_members)}, status=200)
