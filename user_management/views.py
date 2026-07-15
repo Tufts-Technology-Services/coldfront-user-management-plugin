2d7754f83cca1e8f6471217411b3ade274f39ea3
Chris Barnett <Christopher.Barnett@tufts.edu>
2026-07-15 09:27:09 -0400

    handle user serialization in json response

--- user_management/views.py
+++ user_management/views.py
@@ -14,6 +14,6 @@ def get_group_members(request, project_id) -> list[str]:
     project_users = ProjectUser.objects.filter(project_id=project_id, status__name="Active").distinct()
     if project_users.exists():
         for pu in project_users:
-            members.add({"user": pu, "source_match": pu.user.username in ext_members})
+            members.add({"user": {"username": pu.user.username, "first_name": pu.user.first_name, "last_name": pu.user.last_name, "email": pu.user.email}, "source_match": pu.user.username in ext_members})
     missing_members = ext_members - set(project_users.values_list("user__username", flat=True))
     return JsonResponse({"members": list(members), "group": group, "missing_users": list(missing_members)}, status=200)
